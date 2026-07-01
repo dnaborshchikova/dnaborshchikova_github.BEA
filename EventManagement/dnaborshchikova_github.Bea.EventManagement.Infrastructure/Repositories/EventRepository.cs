0f0deb8efedfce2e800784d08b1223c88ce40810
Naborshchikova.DS <Naborshchikova.DS@parma.ru>
2026-07-01 15:02:50 +0300

    #2.7 удаление  тестовых правок

--- EventManagement/dnaborshchikova_github.Bea.EventManagement.Infrastructure/Repositories/EventRepository.cs
+++ EventManagement/dnaborshchikova_github.Bea.EventManagement.Infrastructure/Repositories/EventRepository.cs
@@ -4,6 +4,8 @@ using dnaborshchikova_github.Bea.EventManagement.Core.Models.Exceptions;
 using dnaborshchikova_github.Bea.EventManagement.Infrastructure.ExceptionExtensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Npgsql;
+using System.Text;
 
 namespace dnaborshchikova_github.Bea.EventManagement.Infrastructure
 {
@@ -38,76 +40,32 @@ namespace dnaborshchikova_github.Bea.EventManagement.Infrastructure
 
         public async Task SaveBatchAsync(List<CashRegisterEvent> events)
         {
-            //if (events == null || events.Count == 0)
-            //    return;
+            if (events == null || events.Count == 0)
+                return;
 
-            //var sql = new StringBuilder();
-            //sql.Append(@"INSERT INTO ""Events"" (""Id"", ""Date"", ""UserId"", ""EventType"", ""Data"") VALUES ");
+            var sql = new StringBuilder();
+            sql.Append(@"INSERT INTO ""Events"" (""Id"", ""Date"", ""UserId"", ""EventType"", ""Data"") VALUES ");
 
-            //var parameters = new List<NpgsqlParameter>();
-            //for (int i = 0; i < events.Count; i++)
-            //{
-            //    var e = events[i];
-
-            //    sql.Append($"(@id{i}, @date{i}, @userId{i}, @type{i}, @data{i})");
-
-            //    if (i < events.Count - 1)
-            //        sql.Append(",");
-
-            //    parameters.Add(new NpgsqlParameter($"id{i}", e.Id));
-            //    parameters.Add(new NpgsqlParameter($"date{i}", e.Date));
-            //    parameters.Add(new NpgsqlParameter($"userId{i}", e.UserId));
-            //    parameters.Add(new NpgsqlParameter($"type{i}", e.EventType));
-            //    parameters.Add(new NpgsqlParameter($"data{i}", (object?)e.Data ?? DBNull.Value));
-            //}
-
-            //sql.Append(@" ON CONFLICT (""Id"") DO NOTHING;");
-
-            //await _dbContext.Database.ExecuteSqlRawAsync(sql.ToString(), parameters.ToArray());
-
-            try
+            var parameters = new List<NpgsqlParameter>();
+            for (int i = 0; i < events.Count; i++)
             {
-                _dbContext.Events.AddRange(events);
+                var e = events[i];
 
-                var saved = await _dbContext.SaveChangesAsync();
+                sql.Append($"(@id{i}, @date{i}, @userId{i}, @type{i}, @data{i})");
 
-                _logger.LogInformation("DB save SUCCESS. Written={Count}", saved);
-            }
-            catch (DbUpdateException ex)
-            {
-                _logger.LogError("DB save FAILED (duplicate or constraint violation)");
+                if (i < events.Count - 1)
+                    sql.Append(",");
+
+                parameters.Add(new NpgsqlParameter($"id{i}", e.Id));
+                parameters.Add(new NpgsqlParameter($"date{i}", e.Date));
+                parameters.Add(new NpgsqlParameter($"userId{i}", e.UserId));
+                parameters.Add(new NpgsqlParameter($"type{i}", e.EventType));
+                parameters.Add(new NpgsqlParameter($"data{i}", (object?)e.Data ?? DBNull.Value));
             }
 
-            //try
-            //{
-            //    _dbContext.Events.AddRange(events);
-            //    foreach (var entry in _dbContext.ChangeTracker.Entries<CashRegisterEvent>())
-            //    {
-            //        _logger.LogInformation(
-            //            "Id={Id}, State={State}",
-            //            entry.Entity.Id,
-            //            entry.State);
-            //    }
+            sql.Append(@" ON CONFLICT (""Id"") DO NOTHING;");
 
-            //    _logger.LogInformation("SaveChanges START");
-
-            //    var count = await _dbContext.SaveChangesAsync();
-
-            //    _logger.LogInformation("SaveChanges END ({Count})", count);
-
-            //    var total = await _dbContext.Events.CountAsync();
-            //    _logger.LogInformation("Total in DB: {Count}", total);
-            //}
-            //catch (DbUpdateException ex) when (ex.IsDuplicateKey())
-            //{
-            //    // тут батч уже частично записан
-            //    // просто считаем это "best effort"
-            //}
-            //catch (Exception ex)
-            //{
-            //    // тут батч уже частично записан
-            //    // просто считаем это "best effort"
-            //}
+            await _dbContext.Database.ExecuteSqlRawAsync(sql.ToString(), parameters.ToArray());
         }
     }
 }
